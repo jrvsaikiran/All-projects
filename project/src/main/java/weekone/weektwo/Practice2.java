a3e4373af9a521de30ba74850d403ff3273495e9
jrvsaikiran <saikiranjrv@gmail.com>
2024-05-09 20:34:52 +0530

    practice2

---
+++ project/src/main/java/weekone/weektwo/Practice2.java
@@ -0,0 +1,90 @@
+package weekone.weektwo;
+
+import java.util.*;
+
+public class Practice2 {
+    public static void main(String arg[]){
+
+        Integer[] aray1 = {3,9,5,7,2,2};
+        Arrays.sort(aray1);
+        System.out.println("sort method "+Arrays.toString(aray1));
+
+        //using array reversed comparator
+        Integer[] aray2 = {3,9,5,7,2,2};
+        Arrays.sort(aray2, Comparator.reverseOrder());
+        System.out.println(" array reversed comparator "+Arrays.toString(aray2));
+
+        //sort by collections
+        Integer[] aray3 = {3,9,5,7,2,2};
+        ArrayList<Integer> araylist = new ArrayList<>(Arrays.asList(aray3));
+        System.out.println(araylist.get(1));
+        Collections.sort(araylist);
+        System.out.println("sort by collections "+araylist);
+
+        //sort by streams
+        Integer[] aray4 = {3,9,5,7,2,2};
+        System.out.println("sort by streams");
+        Arrays.stream(aray4).sorted().forEach(System.out::println);
+
+        //max and min in array
+        Integer[] aray5 = {-3,4,5,6,7,-8,2};
+        int max = Integer.MIN_VALUE;
+        int min = Integer.MAX_VALUE;
+
+        for (int i : aray5 ) {
+            if(i>max){
+                max=i;
+            }
+            if(i<min){
+                min=i;
+            }
+        }
+        System.out.println("max element in array "+max);
+        System.out.println("min element in array "+min);
+
+        //to reverse array
+        Integer aray6[] = {5,5,2,1,7,8};
+        int temp,i=0;
+        int j=aray6.length-1;
+        while(i<j){
+            temp = aray6[i];
+            aray6[i] = aray6[j];
+            aray6[j] = temp;
+            i++;
+            j--;
+        }
+        for(int k : aray6){
+            System.out.println("reverse array "+k);
+        }
+//======================================
+        System.out.println("remove and find duplicates of individual in array");
+        Integer[] charArray = {1,4,5,1,44,4,4};
+        LinkedHashMap<Integer, Integer> map = new LinkedHashMap<>();
+        for(int ch : charArray) {
+            if(map.containsKey(ch)) {
+                map.put(ch, map.get(ch)+1);
+            }
+            else
+                map.put(ch, 1);
+        }
+        System.out.println(map);
+        int max1 = Integer.MIN_VALUE;
+        int key = 0;
+        for(Map.Entry<Integer, Integer> entry: map.entrySet()) {
+            if(entry.getValue()>0) {
+                System.out.println(entry.getKey()+"="+entry.getValue());
+            }
+            //most frequent element
+            if(entry.getValue()>max1){
+                max1 = entry.getValue();
+                 key = entry.getKey();
+            }
+        }
+        System.out.println("most frequent element "+key);
+
+
+//===========================================
+    }
+
+
+}
